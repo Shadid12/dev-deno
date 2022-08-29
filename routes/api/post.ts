29c001634c6dcb05d9eb2f39eecb692fec619e43
shadid12 <shadidhaque2012@gmail.com>
2022-08-29 02:11:23 -0400

    all post action done

--- routes/api/post.ts
+++ routes/api/post.ts
@@ -3,7 +3,10 @@ import { RemotePost } from "../../types/Post.ts";
 import { faunaClient, q } from "../../utils/db.ts";
 
 export const handler: Handlers = {
-  async POST(req) {
+  /**
+   * Create a new post
+   */
+  async POST(req: Request) {
     try {
       const body = await req.json();
 
@@ -27,7 +30,31 @@ export const handler: Handlers = {
     }
   },
 
-  async GET() {
+  /**
+   * Get all posts / Get a single post
+   */
+  async GET(req: Request) {
+    const url = new URL(req.url);
+    const id = url.searchParams.get('id');
+
+    // Get a single post
+    if (id) {
+      try {
+        const post = await faunaClient.query(
+          q.Get(q.Ref(q.Collection('Post'), id))
+        );
+
+        return Response.json({
+          data: post.data,
+        });
+      } catch (error) {
+        return Response.json({
+          error: error.message,
+        });
+      }
+    }
+
+    // If no id is provided, return all posts
     try {
       const posts: RemotePost[] = [];
       const { data } = await faunaClient.query(
@@ -50,5 +77,48 @@ export const handler: Handlers = {
         error: error.message,
       });
     }
-  }
+  },
+
+  /**
+   * Update a post
+   */
+  async PUT(req: Request) { 
+    try {
+      const body = await req.json();
+      const post = await faunaClient.query(
+        q.Update(q.Ref(q.Collection('Post'), body._id), {
+          data: {
+            ...body,
+          },
+        })
+      );
+      return Response.json({
+        data: post.data,
+      });
+    } catch (error) { 
+      return Response.json({
+        error: error.message,
+      });
+    }
+  },
+
+  /**
+   * Delete a post
+   */
+  async DELETE(req: Request) {
+    try {
+      const body = await req.json();
+      const post = await faunaClient.query(
+        q.Delete(q.Ref(q.Collection('Post'), body._id))
+      );
+      return Response.json({
+        data: post.data,
+      });
+      
+    } catch (error) {
+      return Response.json({
+        error: error.message,
+      });
+    }
+  },
 };
